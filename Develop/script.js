43161e3c73c14528c845451bdc6f382d2c261af8
Breedlove13 <b.breedlove@outlook.com>
2021-02-12 06:55:48 -0700

    connected slider

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,8 @@
 // Assignment code here
+//window.alert("Welcome to your Password Generator");
+
+
+
 
 
 // Get references to the #generate element
@@ -10,8 +14,40 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+//connecting slider to counter
+
+const slider = document.getElementById("slider");
+const sliderNum = document.getElementById("sliderNumber");
+
+slider.addEventListener("input", sameNum);
+slidernum.addEventListener("input", sameNum);
+
+//connecting function
+
+ function sameNum (num) {
+  const value = num.target.value;
+  slider.value = value;
+  sliderNum.value = value;
+}
+
+
+// var sameNum = (num) => {
+//   const value = num.target.value;
+//   slider.value = value;
+//   slidernum.value = value;
+// };
+
+
+
+
+
+
