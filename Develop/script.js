e832ec5e2919ff01ff99c16cc7414fc06192fad2
Breedlove13 <b.breedlove@outlook.com>
2021-02-12 08:21:24 -0700

    4 rando gens, elemnet id connectors

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,28 @@
 // Assignment code here
 //window.alert("Welcome to your Password Generator");
 
+const password = document.getElementById("password")
+const length = document.getElementById("length")
+const numbers =document.getElementById("numbers")
+const uppercase = document.getElementById("uppercase");
+const lowercase= document.getElementById("lowercase");
+const special = document.getElementById("special");
+const genPassword = document.getElementById("generate");
 
+//adding character generators to an obect containging easy to access single ket: value pairs
+
+var getRandoChars (){
+
+}
+
+
+
+genPassword.addEventListener("click", writePassword);
 
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector;
 
 // Write password to the #password input
 function writePassword() {
@@ -16,8 +32,24 @@ function writePassword() {
   passwordText.value = password;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+function getspecial(){
+specialset = "!@#$%^&*()-_+=<>"
+  return specialset = Math.floor(Math.random() * specialset.length  )
+}
+
+function getNumber(){
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getUppercase(){
+  return String.fromCharCode(Math.floor(Math.random * 26)+ 97 );
+}
+
+function getLowercase (){
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
 
 
 
@@ -40,11 +72,7 @@ slidernum.addEventListener("input", sameNum);
 }
 
 
-// var sameNum = (num) => {
-//   const value = num.target.value;
-//   slider.value = value;
-//   slidernum.value = value;
-// };
+
 
 
 
